9f9eb99cbc1b67c819fde0a01f5827ba266a4508
Ashok Devireddy <53099335+AshokDevireddy@users.noreply.github.com>
2022-10-16 05:08:33 -0700

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+const app = require('./index')
+const serverless = require('serverless-http')
+ 
+//module.exports.hello = serverless(app)
+// module.exports.hello = async (event) => {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify(
+//       {
+//         message: 'Go Serverless v1.0! Your function executed successfully!',
+//         input: event,
+//       },
+//       null,
+//       2
+//     ),
+//   };
+// }
+
+module.exports.list = app.list
+module.exports.add = app.add
+module.exports.remove = app.remove
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+
